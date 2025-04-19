158c06b78f190d1b1d8a75f9e3980f7be629910a
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-04-19 00:46:41 -0400

    Update JSONRecordCreator.py

--- JSONRecordCreator.py
+++ JSONRecordCreator.py
@@ -6,11 +6,15 @@ class JSONGrapherRecord:
     This class enables making JSONGrapher records. Each instance represents a structured JSON record for a graph.
     One can optionally provide an existing JSONGrapher record during creation to pre-populate the object.
 
-    Attributes:
-        comments: General description or metadata related to the entire record. Can include citation links.
-        title: Title of the graph or the dataset being represented.
-        data: A list of data series, where each series is a dictionary containing
-              relevant information like x/y values, comments, and styling details.
+    Arguments & Attributes (all are optional):
+        comments (str): General description or metadata related to the entire record. Can include citation links.
+        data_type: The data_type is the experiment type or similar, it is used to assess which records can be compared and which (if any) schema to compare to. This ends up being the top level title field of the full JSONGrapher file.
+        graph_title: Title of the graph or the dataset being represented.
+        data_objects_list (list): List of data series dictionaries to pre-populate the record. 
+        x_data: Single series x data in a list or array-like structure. 
+        y_data: Single series y data in a list or array-like structure.
+        x_axis_label_including_units: A string with units provided in parentheses. Use of multiplication "*" and division "/" and parentheses "( )" are allowed within in the units .
+        y_axis_label_including_units: A string with units provided in parentheses. Use of multiplication "*" and division "/" and parentheses "( )" are allowed within in the units .
         layout: A dictionary defining the layout of the graph, including axis titles,
                 comments, and general formatting options.
     
@@ -21,28 +25,24 @@ class JSONGrapherRecord:
         populate_from_existing_record: Populates the attributes from an existing JSONGrapher record.
     """
     
-    def __init__(self, comments="", title="", data=None, layout=None, existing_JSONGrapher_record=None):
+    def __init__(self, comments="", graph_title="", data_type="", data_objects_list = [], x_data=None, y_data=None, x_axis_label_including_units="", y_axis_label_including_units ="",  layout={}, existing_JSONGrapher_record=None):
         """
         Initialize a JSONGrapherRecord instance with optional attributes or an existing record.
 
-        Args:
-            comments (str): General description or metadata for the record.
-            title (str): Title of the graph or dataset.
-            data (list): List of data series dictionaries to pre-populate the record.
             layout (dict): Layout dictionary to pre-populate the graph configuration.
             existing_JSONGrapher_record (dict): Existing JSONGrapher record to populate the instance.
         """
         # Default attributes for a new record.
-        self.comments = comments  # Description or metadata for the record.
-        self.title = title  # Title of the graph or dataset.
-        self.data = data if data is not None else []  # List of data series.
-        self.layout = layout if layout is not None else {
-            "comments": "",  # Comments specific to the layout.
-            "title": "",  # Title of the graph.
-            "xaxis": {"comments": "", "title": ""},  # x-axis configuration.
-            "yaxis": {"comments": "", "title": ""}   # y-axis configuration.
-        }
-        
+        self.comments = comments # Description and metadata for the record goes into the top level comments field.
+        self.title = data_type  #The data_type is the experiment type or similar, it is used to assess which records can be compared and which (if any) schema to compare to. This ends up being the top level title field of the full JSONGrapher file.
+        self.data = data_objects_list
+        self.layout = layout
+        if len(graph_title) > 0:
+            self.layout["title"] = graph_title
+        if len(x_axis_label_including_units) > 0:
+            self.layout["title"] = graph_title
+        if len(y_axis_label_including_units) > 0:
+            self.layout["title"] = graph_title
         # Populate attributes if an existing JSONGrapher record is provided.
         if existing_JSONGrapher_record:
             self.populate_from_existing_record(existing_JSONGrapher_record)
@@ -50,54 +50,86 @@ class JSONGrapherRecord:
     def populate_from_existing_record(self, existing_JSONGrapher_record):
         """
         Populates attributes from an existing JSONGrapher record.
-
-        Args:
-            existing_JSONGrapher_record: A dictionary representing an existing JSONGrapher record.
+        existing_JSONGrapher_record: A dictionary representing an existing JSONGrapher record.
         """
-        self.comments = existing_JSONGrapher_record.get("comments", self.comments)
-        self.title = existing_JSONGrapher_record.get("title", self.title)
-        self.data = existing_JSONGrapher_record.get("data", self.data)
-        self.layout = existing_JSONGrapher_record.get("layout", self.layout)
+        if "comments" in existing_JSONGrapher_record: self.comments = existing_JSONGrapher_record["comments"]
+        if "title" in existing_JSONGrapher_record: self.title = existing_JSONGrapher_record["title"]
+        if "data" in existing_JSONGrapher_record: self.data = existing_JSONGrapher_record["data"]
+        if "layout" in existing_JSONGrapher_record: self.layout = existing_JSONGrapher_record["layout"]
 
-    def add_data_series(self, comments, name, data_type, x, y, uid="", line="", extra_fields=None):
-        # comments: Description of the data series.
-        # uid: Optional unique identifier for the series (e.g., a DOI).
-        # line: Dictionary describing line properties (e.g., shape, width).
-        # name: Name of the data series.
-        # data_type: Type of the data (e.g., scatter, line).
+    def set_graph_title(self, graph_title):
+        """
+        Updates the title of the graph in the layout dictionary.
+        graph_title (str): The new title to set for the graph.
+        """
+        self.layout["title"] = graph_title
+
+
+    def set_x_axis_label(self, x_axis_label_including_units):
+        """
+        Updates the title of the x-axis in the layout dictionary.
+        xaxis_title (str): The new title to set for the x-axis.
+        """
+        if "xaxis" not in self.layout or not isinstance(self.layout.get("xaxis"), dict):
+            self.layout["xaxis"] = {}  # Initialize x-axis as a dictionary if it doesn't exist.
+        
+        self.layout["xaxis"]["title"] = x_axis_label_including_units
+
+
+    def set_y_axis_label(self, y_axis_label_including_units):
+        """
+        Updates the title of the y-axis in the layout dictionary.
+        yaxis_title (str): The new title to set for the y-axis.
+        """
+        if "yaxis" not in self.layout or not isinstance(self.layout.get("yaxis"), dict):
+            self.layout["yaxis"] = {}  # Initialize y-axis as a dictionary if it doesn't exist.
+        
+        self.layout["yaxis"]["title"] = y_axis_label_including_units
+
+
+    def add_data_series(self, series_name, x=[], y=[], simulate=None, comments="", plotting_style="",  uid="", line="", extra_fields=None):
+        """
+        This is the normal way of adding an x,y data series.
+        """
+        # series_name: Name of the data series.
         # x: List of x-axis values.
         # y: List of y-axis values.
+        # simulate: This is an optional field which, if used, is a JSON object with entries for calling external simulation scripts.
+        # comments: Optional description of the data series.
+        # plotting_style: Type of the data (e.g., scatter, line).
+        # line: Dictionary describing line properties (e.g., shape, width).
+        # uid: Optional unique identifier for the series (e.g., a DOI).
         # extra_fields: Dictionary containing additional fields to add to the series.
-        
         series = {
+            "name": series_name,
+            "type": plotting_style,
+            "x": x,
+            "y": y,
             "comments": comments,
             "uid": uid,
             "line": line,
-            "name": name,
-            "type": data_type,
-            "x": x,
-            "y": y,
         }
-        
-        # Add extra fields if provided
+        #add simulate field if included.
+        if simulate != None:
+            series["simulate"] = simulate
+        # Add extra fields if provided, they will be added.
         if extra_fields:
             series.update(extra_fields)
-
-        # Finally, add to the class object.
+        # Finally, add to the class object's data list.
         self.data.append(series)
 
-    def set_layout(self, comments, title, xaxis_title, xaxis_comments, yaxis_title, yaxis_comments):
+    def set_layout(self, comments="", graph_title="", x_axis_label_including_units="", y_axis_label_including_units="", x_axis_comments="",y_axis_comments=""):
         # comments: General comments about the layout.
-        # title: Title of the graph.
+        # graph_title: Title of the graph.
         # xaxis_title: Title of the x-axis, including units.
         # xaxis_comments: Comments related to the x-axis.
         # yaxis_title: Title of the y-axis, including units.
         # yaxis_comments: Comments related to the y-axis.
         self.layout = {
             "comments": comments,
-            "title": title,
-            "xaxis": {"comments": xaxis_comments, "title": xaxis_title},
-            "yaxis": {"comments": yaxis_comments, "title": yaxis_title}
+            "title": graph_title,
+            "xaxis": {"title": x_axis_label_including_units, "comments": x_axis_comments},
+            "yaxis": {"title": y_axis_label_including_units, "comments": y_axis_comments,}
         }
     
     def to_json(self, filepath):
@@ -118,32 +150,19 @@ if __name__ == "__main__":
     # Example of creating a record with optional attributes.
     record = JSONGrapherRecord(
         comments="Here is a description.",
-        title="ACS Division Records",
-        data=[
+        graph_title="Graph Title",
+        data_objects_list=[
             {"comments": "Initial data series.", "uid": "123", "line": {"shape": "solid"}, "name": "Series A", "type": "line", "x": [1, 2, 3], "y": [4, 5, 6]}
         ],
-        layout={
-            "comments": "Predefined layout comments.",
-            "title": "Predefined Layout Title",
-            "xaxis": {"comments": "Predefined X-axis comments", "title": "Predefined X-axis Title"},
-            "yaxis": {"comments": "Predefined Y-axis comments", "title": "Predefined Y-axis Title"}
-        }
     )
-    print(record.comments)  # Outputs: Here is a description
-    print(record.data)      # Outputs: [{'comments': 'Initial data series.', 'uid': '123', ...}]
-    
+
     # Example of creating a record from an existing dictionary.
     existing_JSONGrapher_record = {
         "comments": "Existing record description.",
-        "title": "Existing Graph",
+        "graph_title": "Existing Graph",
         "data": [
             {"comments": "Data series 1", "uid": "123", "line": {"shape": "solid"}, "name": "Series A", "type": "line", "x": [1, 2, 3], "y": [4, 5, 6]}
         ],
-        "layout": {
-            "comments": "Layout comments",
-            "title": "Existing Layout Title",
-            "xaxis": {"comments": "Existing X-axis comments", "title": "Existing X-axis Title"},
-            "yaxis": {"comments": "Existing Y-axis comments", "title": "Existing Y-axis Title"}
-        }
     }
     record_from_existing = JSONGrapherRecord(existing_JSONGrapher_record=existing_JSONGrapher_record)
+    record.to_json("test.json")

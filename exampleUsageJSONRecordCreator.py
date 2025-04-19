e7eb02db3dab4cfb3d1902accf2e135e2dc1a9f0
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-04-19 10:04:13 -0400

    Update exampleUsageJSONRecordCreator.py

--- exampleUsageJSONRecordCreator.py
+++ exampleUsageJSONRecordCreator.py
@@ -9,9 +9,16 @@ except:
     import JSONRecordCreator #this is if you have the class file locally.
 
 
-#While one can call the JSONGrapherRecord class directly, for some people
-# it is easier to remember to use create_new_JSONGrapherRecord()
-#Additionally, it is easier to see the default fields by printing, after that.
+print("\n\n CREATING AN EMPTY RECORD")
+#it is easiest to start with create_new_JSONGrapherRecord(). 
+# While one can create an instance of the JSONGrapherRecord class directly, 
+#This function is easy to remember. We can print to see the default fields, after that.
 new_record = JSONRecordCreator.create_new_JSONGrapherRecord()
+print(new_record)
 
+print("\n\n CREATING AN EMPTY RECORD WITH HINTS")
+#now, let's make a new record
+# the create_new_JSONGrapherRecord function creates a new record that is not actually blank, 
+#it includes strings as instructions for fields that should be filled in by the user.
+new_record = JSONRecordCreator.create_new_JSONGrapherRecord(hints=True)
 print(new_record)

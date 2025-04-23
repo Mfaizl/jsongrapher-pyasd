1461a413669c0fbd067d6452a9ce2eba26a7052a
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-04-23 19:40:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,12 +20,12 @@ URL = 'https://github.com/AdityaSavara/JSONGrapherRC'
 EMAIL = 'AditySavara2008@u.northwestern.edu'  
 AUTHOR = 'Aditya Savara'
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = '1.1'
+VERSION = '1.2'
 LICENSE = 'Unlicense'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'numpy', 'json'
+    'numpy' 
     # 'requests', 'maya', 'records', #numpy...
 ]
 

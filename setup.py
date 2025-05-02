bcc0c6aefe7abfdbaee80002833be281491b8725
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-05-02 00:00:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = 'https://github.com/AdityaSavara/JSONGrapherRC'
 EMAIL = 'AditySavara2008@u.northwestern.edu'  
 AUTHOR = 'Aditya Savara'
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = '1.7'
+VERSION = '1.8'
 LICENSE = 'Unlicense'
 
 # What packages are required for this module to be executed?

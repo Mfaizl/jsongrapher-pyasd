65e7a8b6380eaa7985a09564803914e769e4c15f
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-05-11 16:55:59 -0400

    Update example_2_drag_and_drop_records.py

--- example/example2/example_2_drag_and_drop_records.py
+++ example/example2/example_2_drag_and_drop_records.py
@@ -6,10 +6,16 @@ import JSONGrapherRC
 
 global_records_list = []
 
+#The below class creates a window for dragging and dropping or browsing and selecting files
+#And each time a file is added the full file list and most recent file will be passed to
+#The function supplied by the user (function_for_after_file_addition)
+#This class **cannot** be initiated directly, it should initiated using the
+#companion function create_and_launch
 class DragDropApp:
-    def __init__(self, root):
+    def __init__(self, root, app_name = '', function_for_after_file_addition = None):
         self.root = root
-        self.root.title("JSONGrapher")
+        self.root.title(app_name)
+        self.function_for_after_file_addition = function_for_after_file_addition
 
         # Enable native drag-and-drop capability
         self.root.drop_target_register(DND_FILES)
@@ -40,6 +46,7 @@ class DragDropApp:
         """Clears the listbox and resets selected files."""
         self.file_listbox.delete(0, tk.END)  # Clear listbox
         self.selected_files = []  # Reset file list
+        self.function_for_after_file_addition(filelist=[], newest_file_name_and_path='')
         print("File list cleared!")  # Optional debug message
 
     def open_file_dialog(self):
@@ -61,34 +68,59 @@ class DragDropApp:
         self.file_listbox.delete(0, tk.END) # Clear listbox
         for filename_and_path in self.selected_files:
             self.file_listbox.insert(tk.END, os.path.basename(filename_and_path)) # Show filenames only
-        print("line 55", filename_and_path)
-        if len(global_records_list) == 0:
-            first_record = JSONGrapherRC.create_new_JSONGrapherRecord()
-            first_record.import_from_file(filename_and_path)
-            #index 0 will be the one we merge into.
-            global_records_list.append(first_record)
-            #index 1 will be where we store the first record, so we append again.
-            global_records_list.append(first_record)
-        else:
-            current_record = JSONGrapherRC.create_new_JSONGrapherRecord()
-            current_record.import_from_file(filename_and_path)
-            global_records_list.append(current_record)
-            #now create merged record.
-            global_records_list[0] = JSONGrapherRC.merge_JSONGrapherRecords([global_records_list[0], current_record])
-        #plot the index 0, which is the most up to date merged record.
-        global_records_list[0].plot_with_plotly()
+        #If the app has a function to call, we'll send the newest file in.
+        if self.function_for_after_file_addition != None:
+            #pass in the selected_files and the most recent filename_and_path
+            self.function_for_after_file_addition(self.selected_files, filename_and_path)
 
     def finish_selection(self):
         """Closes the window and returns selected files."""
         self.root.quit() # Close the window
 
-def launch():
+# This function is a companion function to
+# The class DragDropApp for creating a file selection and function call app
+def create_and_launch(app_name = '', function_for_after_file_addition=None):
     """Starts the GUI and returns selected files."""
     root = TkinterDnD.Tk()
-    app = DragDropApp(root)
+    app = DragDropApp(root, app_name=app_name, function_for_after_file_addition=function_for_after_file_addition)
     root.mainloop() # Runs the Tkinter event loop
     return app.selected_files # Returns selected files after the window closes
+
+#This is a JSONGrapher specific function
+#That takes filenames and adds new JSONGrapher records to a global_records_list
+#If the filelist and newest_file_name_and_path are [] and '', that means to clear the global_records_list.
+def add_records_to_global_records_list_and_plot(filelist, newest_file_name_and_path, plot_immediately=True):
+    #First check if we have received a "clear" condition.
+    if (len(filelist) == 0) and (newest_file_name_and_path == ''):
+        global_records_list.clear()
+        return global_records_list
+    filename_and_path = newest_file_name_and_path
+    if len(global_records_list) == 0:
+        first_record = JSONGrapherRC.create_new_JSONGrapherRecord()
+        first_record.import_from_file(filename_and_path)
+        #index 0 will be the one we merge into.
+        global_records_list.append(first_record)
+        #index 1 will be where we store the first record, so we append again.
+        global_records_list.append(first_record)
+    else:
+        current_record = JSONGrapherRC.create_new_JSONGrapherRecord()
+        current_record.import_from_file(filename_and_path)
+        global_records_list.append(current_record)
+        #now create merged record.
+        global_records_list[0] = JSONGrapherRC.merge_JSONGrapherRecords([global_records_list[0], current_record])
+    if plot_immediately:
+        #plot the index 0, which is the most up to date merged record.
+        global_records_list[0].plot_with_plotly()
+    return global_records_list
+
+
+#This ia JSONGrapher specific wrapper function to create_and_launch.
+def launch():
+    selected_files = create_and_launch(app_name = "JSONGRapher", function_for_after_file_addition=add_records_to_global_records_list_and_plot)
+    #We will not return the selected_files, and instead will return the global_records_list.
+    return global_records_list
 
 # Example usage:
 files = launch()
 print("Selected files:", files)
+

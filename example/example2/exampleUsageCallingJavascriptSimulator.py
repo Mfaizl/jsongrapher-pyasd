e3f067aa5077797bd2ea7d96289b2093339e5556
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-05-11 14:29:44 -0400

    Update exampleUsageCallingJavascriptSimulator.py

--- example/example2/exampleUsageCallingJavascriptSimulator.py
+++ example/example2/exampleUsageCallingJavascriptSimulator.py
@@ -22,14 +22,27 @@ Record_with_simulate_field.plot_with_plotly()
 print("The data has been simulated using a javascript function, with source code called from online.")
 
 #One can change the parameters and then update the data object by forcing simulation again. Below is an example.
+#Let's first copy the record we just had.
+import copy
+adjusted_Record_with_simulate_field = copy.deepcopy(Record_with_simulate_field)
 #There is only a single data series, so it is index 0. We'll change the rate constant and sigma_max in the simulate field
-Record_with_simulate_field.fig_dict["data"][0]["simulate"]["K_eq"] = "50.3 (1/bar)"
-Record_with_simulate_field.fig_dict["data"][0]["simulate"]["sigma_max"] =  ".4267670459667 (mol/kg)"
-#Now call a function to force re-simulation of the data series at index 0.
-Record_with_simulate_field.simulate_data_series_by_index(data_series_index=0)
-#Now plot again.
-Record_with_simulate_field.plot_with_plotly()
+adjusted_Record_with_simulate_field.fig_dict["data"][0]["simulate"]["K_eq"] = "50.3 (1/bar)"
+adjusted_Record_with_simulate_field.fig_dict["data"][0]["simulate"]["sigma_max"] =  ".4267670459667 (mol/kg)"
+#Don't forget to change the name of the dataseries now that we have changed the data inside of it.
+adjusted_Record_with_simulate_field.fig_dict["data"][0]["name"] = "CO2 Adsorption, K_eq = 50.3 (1/bar)"
 
+#Now call a function to force re-simulation of the data series at index 0.
+adjusted_Record_with_simulate_field.simulate_data_series_by_index(data_series_index=0)
+#Now plot again.
+adjusted_Record_with_simulate_field.plot_with_plotly()
+
+#Now let's plot both records together by merging them.
+import JSONGrapherRC
+merged_record = JSONGrapherRC.merge_JSONGrapherRecords([Record_with_simulate_field, adjusted_Record_with_simulate_field])
+#plotting with plotly.
+merged_record.plot_with_plotly()
+#plotting with matplotlib.
+merged_record.plot_with_matplotlib()
 
 
 

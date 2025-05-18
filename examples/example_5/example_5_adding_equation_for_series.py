a9fe4751af52b9f1796f25991f9338e74e3d67a3
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-05-18 01:14:48 -0400

    Update example_5_adding_equation_for_series.py

--- examples/example_5/example_5_adding_equation_for_series.py
+++ examples/example_5/example_5_adding_equation_for_series.py
@@ -9,7 +9,9 @@ Record.set_graph_title("Typical Temperature Behavior for a Rate Constant")
 Record.set_x_axis_label_including_units("T (K)")
 Record.set_y_axis_label_including_units("k (s**(-1))")
 
-#Now, let's define an equation, with parameters, using the equation_dict format of json_equationer.
+### Let's add an equation_dict ###
+
+#We'll define an equation, with parameters using the equation_dict format that json_equationer uses.
 #The later variables define the default range and points resolution of the curve.
 equation_dict = {
     'equation_string': 'k = A*(e**((-Ea)/(R*T)))',
@@ -26,8 +28,33 @@ equation_dict = {
 
 #Now we can add this equation record as a dataseries into the JSONGrapher record. 
 # By default, the equation will be evaluated when added, and also when plotted.
-Record.add_data_series_as_equation(series_name="Arrhenius Example",equation_dict=equation_dict)
-
+Record.add_data_series_as_equation(series_name="Arrhenius Example 1",equation_dict=equation_dict)
 Record.plot_with_plotly()
 
+### Now, let's add an equation using json_equationer ###
 
+#one can use import json_equationer.equation_creator, but it is also packaged with JSONGrapher for convenience.
+import JSONGrapher.equation_creator as equation_creator
+second_Arrhenius_equation = equation_creator.Equation()
+second_Arrhenius_equation.set_x_variable("T (K)")  # Temperature in Kelvin
+second_Arrhenius_equation.set_y_variable("k (s**-1)")  # Rate constant in inverse seconds
+second_Arrhenius_equation.set_equation("k = A * (e ** (-Ea / (R * T)))")
+# Add constants one at a time, or through a list. We'll use a different Ea for this one.
+second_Arrhenius_equation.add_constants({"Ea": "40000 J/mol"})  
+second_Arrhenius_equation.add_constants([
+    {"R": "8.314 J/(mol*K)"},
+    {"A": "1*10**13 (s**-1)"},
+    {"e": "2.71828"}  # No unit required
+])
+# Optinally, set minimum number of points and limits for calculations.
+second_Arrhenius_equation.set_num_of_points(10)
+second_Arrhenius_equation.set_x_range_default([200, 500])
+second_Arrhenius_equation.set_x_range_limits([None, 600])  
+
+# Define additional properties.
+second_Arrhenius_equation.equation_dict["points_spacing"] = "Linear"
+
+#Use the equation dictionary to add the second data series, then plot
+Record.add_data_series_as_equation(series_name="Arrhenius Example2",equation_dict=second_Arrhenius_equation.equation_dict)
+
+Record.plot_with_plotly()

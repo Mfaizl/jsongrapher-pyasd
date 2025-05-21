dba031f8496db21d9404159368650da2593e2797
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-05-20 20:29:30 -0400

    Creating Example 6

---
+++ examples/example_6/example_6_equations_and_points.py
@@ -0,0 +1,8 @@
+import JSONGrapher
+
+#Example showing datapoints and equations on the same graph.
+#The ranges for the equations are evaluated automatically by JSONGrapher.
+#When making equations, one thing to be careful about is that a "variable" or a "constant" is required for each term that has units in the equation_string.
+
+example_record = JSONGrapher.merge_JSONGrapherRecords(["O_OH_100.json","O_OH_100_relation.json","O_OH_110.json","O_OH_110_relation.json" ])
+example_record.plot_with_plotly()

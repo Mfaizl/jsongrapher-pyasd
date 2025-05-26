4f479d0a3e36649619349ee6464d4d24777e9fa0
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-05-26 16:27:55 -0400

    Adding bubble plot to example 1

--- examples/example_1/example_1_drag_and_drop_records.py
+++ examples/example_1/example_1_drag_and_drop_records.py
@@ -9,10 +9,11 @@ final_plots_record_list = JSONGrapher.launch()
 
 ##STEP 3: Click "Clear Files List", then drag in O_OH_Scaling.json to see a fancy graph.
 ##STEP 4: Click "Clear Files List", then drag in SrTiO3_rainbow.json to see a fancy graph.
-##STEP 5: Click "Clear Files List", then drag in Rate_Constant_mesh3d to see a 3D surface plot
-##STEP 6: Click "Clear Files List", then drag in Rate_Constant_scatter3d to see a 3D scatter plot
-##STEP 7: Click "Clear Files List", then drag in LaMnO3.json and LaFeO3.json
-##STEP 8: Click "End", this will return the merged record of the final plot.
+##STEP 5: Click "Clear Files List", then drag in Rate_Constant_mesh3d.json to see a 3D surface plot #This will calculate values on the fly and take a minute. Feel free to skip
+##STEP 6: Click "Clear Files List", then drag in Rate_Constant_scatter3d_edited.json to see a 3D scatter plot #This will calculate values on the fly and take a minute. Feel free to skip
+##STEP 7: Click "Clear Files List", then drag in Rate_Constant_bubble.json to see a bubble plot #This will calculate values on the fly and take a minute. Feel free to skip
+##STEP 8: Click "Clear Files List", then drag in LaMnO3.json and LaFeO3.json
+##STEP 9: Click "End", this will return the merged record of the final plot.
 
 #The final_plot_record is the first item in the final_plot_records_list. It combines all the data (all series) from in the most recent plot into one JSONGrapher Record.
 #The ability to merge records, even from different starting units, is one of the major features of JSONGrapher.

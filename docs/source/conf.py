b51ebc0419c0cf1e4e49084d78dc31764946b3f2
M Fazil <96303518+Mfaizl@users.noreply.github.com>
2025-08-18 19:58:48 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Add the parent directory to sys.path so Sphinx can find the source code
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../JSONGrapher'))
 
 # Project information
 project = 'JsonGrapher'

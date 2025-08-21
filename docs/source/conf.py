2a6bd668f14be938166679f0aa240b0bad11a278
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-08-20 20:14:44 -0400

    Create conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+# Add the parent directory to sys.path so Sphinx can find the source code
+sys.path.insert(0, os.path.abspath('../../'))
+
+# Project information
+project = 'JsonGrapher'
+copyright = '2025, JsonGrapher'
+author = 'JsonGrapher'
+release = '0.1'
+
+# Extensions to enable
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc"
+]
+
+# HTML theme to use
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
